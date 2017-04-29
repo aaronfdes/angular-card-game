8f855565f6ee3d3439218ec95391f10bbc2a53e4
Aaron Fernandes <aaronfernandes1406@gmail.com>
2017-04-29 08:53:43 +0530

    Issue 2: Fixed points 2 and 4

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Card } from './card';
 
 @Component({
@@ -11,8 +11,15 @@ export class CardComponent implements OnInit {
   @Input()
   private card: Card;
 
+  @Output()
+  private clickedCardEmitter: EventEmitter<Card> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onCardClick(){
+    this.clickedCardEmitter.emit(this.card);
   }
 }
